da6e5399325796d7f770797e1abc390ca3b23228
Kim Kruse <kim@kimkruse.xyz>
2022-04-13 08:49:52 +0200

    changes first commit

---
+++ emacs/elpa/vterm-20220412.1140/vterm-pkg.el
@@ -0,0 +1,12 @@
+(define-package "vterm" "20220412.1140" "Fully-featured terminal emulator"
+  '((emacs "25.1"))
+  :commit "a2f2286c40725244257f46d3a27a730e73f9f596" :authors
+  '(("Lukas Fürmetz" . "fuermetz@mailbox.org"))
+  :maintainer
+  '("Lukas Fürmetz" . "fuermetz@mailbox.org")
+  :keywords
+  '("terminals")
+  :url "https://github.com/akermu/emacs-libvterm")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
